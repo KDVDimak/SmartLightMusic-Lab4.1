4454edd41d115e2fb868ddb08cd80023889b5b95
KDVDimak <162099821+KDVDimak@users.noreply.github.com>
2025-11-09 22:51:56 +0200

    ProgramCode.cs

---
+++ src/ProgramCode.cs
@@ -0,0 +1,11 @@
+using System;
+namespace SmartLightMusicLab4
+{
+    public class ProgramCode
+    {
+        public static void Main()
+        {
+            Console.WriteLine("SmartLightMusicLab4 Product");
+        }
+    }
+}
